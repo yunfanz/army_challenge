c6173fc0480ab993ed0559763a701d4be1935fa6
Yunfan Zhang <yunfanz@berkeley.edu>
2018-07-22 22:51:34 -0700

    save

--- Code/HiearchicalInference.py
+++ Code/HiearchicalInference.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser(description='Process')
 parser.add_argument('--train_dir', type=str, default='./log/model_9/',
                     help='an integer for the accumulator')
 parser.add_argument('--all_snr', type=bool, default=False)
+parser.add_argument('--eps', type=float, default=1.e-15)
 parser.add_argument('--model', type=str, default=None,
                     help='an integer for the accumulator')
 parser.add_argument('--submodel', type=str, default=None,
@@ -29,7 +30,7 @@ parser.add_argument('--data_format', type=str, default="channels_last",
 parser.add_argument('--mode', type=str, default='test')
 args = parser.parse_args()
 
-EPS = 1.e-15
+EPS = args.eps#1.e-15
 CLASSES = ['16PSK', '2FSK_5KHz', '2FSK_75KHz', '8PSK', 'AM_DSB', 'AM_SSB', 'APSK16_c34',
  'APSK32_c34', 'BPSK', 'CPFSK_5KHz', 'CPFSK_75KHz', 'FM_NB', 'FM_WB',
  'GFSK_5KHz', 'GFSK_75KHz', 'GMSK', 'MSK', 'NOISE', 'OQPSK', 'PI4QPSK', 'QAM16',
@@ -137,6 +138,9 @@ else:
             sub_sum = np.sum(preds[i,mods])
             sub_hat = subpreds[i]
             preds[i,mods] = sub_sum * sub_hat
+    preds = np.where(preds>EPS, preds, EPS)
+    preds = np.where(preds<1-EPS, preds, 1-EPS)
+    preds /= np.sum(preds, axis=1, keepdims=True)
     # save with 15 decimals
     fmt = '%1.0f' + preds.shape[1] * ',%1.15f'
     id_col = np.arange(1, testdata.shape[0] + 1)
