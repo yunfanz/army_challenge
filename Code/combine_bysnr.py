8f22e50f55405e9957f3ad51cc21ae02953a836f
Gerry Zhang <yzhang@blc00.bl.pvt>
2018-08-10 15:40:23 +0000

    save

--- Code/combine_bysnr.py
+++ Code/combine_bysnr.py
@@ -2,26 +2,14 @@ from score import *
 
 csv_path1 = '/home/yzhang/Projects/army_challenge/Code/anom/test2_1262gp0.csv'
 csv_path2 = '/home/yzhang/Projects/army_challenge/Code/anom/test2_1121gp0.csv'
-#csv_path2 = './anom/train0_1121_model0'
 
-# snr = np.load('/datax/yzhang/test_data/train0_snr.npy')
 snr = np.load('/datax/yzhang/test_data/snr_preds2.npy')
 snr = np.argmax(snr, axis=1)
 
 snr_inds = [np.argwhere(snr==S).squeeze() for S in range(6)]
 pred1 = get_pred(csv_path1)
 pred2 = get_pred(csv_path2)
-#labels = load_label('/datax/yzhang/training_data/training_data_chunk_0.pkl')
 lambs = [0.75,0.75,0.85,0.9,0.95,0.95]
-
-#for S in range(6):
-#    inds = snr_inds[S]
-#    print()
-#    print(S,':')
-#    for L in np.arange(0,1.05,0.05):
-#        logloss, score = evaluate(pred1[inds]*L+pred2[inds]*(1-L), labels[inds])
-#        print(L, logloss, score)
-#print(snr.shape, pred1.shape, pred2.shape)
 
 
 res = np.empty(pred1.shape)
@@ -32,7 +20,7 @@ for i,lmbda in enumerate(lambs):
 
 
     
-fmt = '%1.0f' + res.shape[1] * ',%1.15f'
+fmt = '%1.0f' + res.shape[1] * ',%1.8f'
 id_col = np.arange(1, res.shape[0] + 1)
 preds = np.insert(res, 0, id_col, axis = 1)    
     
@@ -43,7 +31,7 @@ CLASSES = ['16PSK', '2FSK_5KHz', '2FSK_75KHz', '8PSK', 'AM_DSB', 'AM_SSB', 'APSK
     'GFSK_5KHz', 'GFSK_75KHz', 'GMSK', 'MSK', 'NOISE', 'OQPSK', 'PI4QPSK', 'QAM16',
     'QAM32', 'QAM64', 'QPSK']
 
-header = "ID,"
+header = "Index,"
 for i in range(len(CLASSES) - 1):
     header += CLASSES[i]+','
 header += CLASSES[-1] + '\n'
